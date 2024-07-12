33a380237e5ae73b32b2fcb0298846fc7ea901f2
sandeep0009 <500sandeepks@gmail.com>
2024-07-12 13:11:19 +0530

    done with home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import Typewriter from 'typewriter-effect';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    const navigate=useNavigate();
+    const handleNavigate=async()=>{
+        navigate('/create-invoice')
+    }
+    return (
+        <div className='flex flex-col justify-center items-center h-screen'>
+            <div className='text-2xl font-bold'>
+                <Typewriter
+                    options={{
+                        strings: ['Welcome to the invoice assignment'],
+                        autoStart: true,
+                        loop: true,
+                    }}
+                />
+            </div>
+
+
+          <button className='mt-5 bg-black text-white w-28 h-14 rounded-lg' onClick={handleNavigate}>Click to Start</button>
+        </div>
+    )
 }
 
 export default Home
